cba1dbc434a13f4c4c8559fe2f46b91bca2695d7
samuel henrique <samuelhenrique_08@hotmail.com>
2020-02-27 08:25:18 -0300

    aplicativo inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { QRScanner } from '@ionic-native/qr-scanner/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
